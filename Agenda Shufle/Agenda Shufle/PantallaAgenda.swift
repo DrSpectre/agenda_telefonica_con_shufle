bf394ef7df04af7d94362d86ee7bbbfb0cf68ce5
Jadzia Eleonore <bluakorvo@protonmail.com>
2025-02-26 10:45:58 -0700

    [MODIFICA] la pantalla agenda para usar el modelo nuevo

--- Agenda Shufle/Agenda Shufle/PantallaAgenda.swift
+++ Agenda Shufle/Agenda Shufle/PantallaAgenda.swift
@@ -7,12 +7,28 @@
 
 import SwiftUI
 
+
+let contactos = [
+    ContactoAgenda(nombre: "Juna", telefono: "12345"),
+    ContactoAgenda(nombre: "Jun", telefono: "12345"),
+    ContactoAgenda(nombre: "Ja", telefono: "12345"),
+    ContactoAgenda(nombre: "Juas", telefono: "12345"),
+    ContactoAgenda(nombre: "Junsda", telefono: "12345"),
+    ContactoAgenda(nombre: "Judsana", telefono: "12345"),
+    ContactoAgenda(nombre: "Juasdna", telefono: "12345"),
+    ContactoAgenda(nombre: "Jusdana", telefono: "12345"),
+    ContactoAgenda(nombre: "Judsana", telefono: "12345"),
+    ContactoAgenda(nombre: "Judsana", telefono: "12345"),
+]
+
+
 struct PantallaAgenda: View {
     var body: some View {
 
         List {
             VStack {
-                ForEach(0...45, id: \.self){ _ in
+                ForEach(contactos){ contacto in
+                    Text("\(contacto.nombre)")
                     ContactoPrevista()
                 }
             }
