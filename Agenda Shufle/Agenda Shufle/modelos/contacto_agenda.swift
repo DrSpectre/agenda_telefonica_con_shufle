182dcc561fd375ad071e5d2a3ab7478004f5d872
Jadzia Eleonore <bluakorvo@protonmail.com>
2025-02-26 10:45:14 -0700

    [AGREGA] un modelo de contacto para guardar informacion

---
+++ Agenda Shufle/Agenda Shufle/modelos/contacto_agenda.swift
@@ -0,0 +1,28 @@
+//
+//  contacto_agenda.swift
+//  Agenda Shufle
+//
+//  Created by Jadzia Gallegos on 26/02/25.
+//
+
+import SwiftUI
+
+struct ContactoAgenda: Identifiable {
+    var nombre: String
+    var telefono: String
+    
+    var id: String { nombre }
+}
+
+/*
+var nombre = "Pepito Veraz"
+
+var _nombre: String
+
+var nombre: set{
+    let nombre_validado = filtrar_palabras(value)
+    _nombre = nombre_validado
+} get {
+    return _nombre.split(" ")[0]
+}
+*/
