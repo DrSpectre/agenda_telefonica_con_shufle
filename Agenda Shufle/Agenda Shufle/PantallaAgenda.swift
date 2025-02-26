a6cb2c44dd624d0374697ba3807879b120c2c325
Jadzia Eleonore <bluakorvo@protonmail.com>
2025-02-26 10:13:44 -0700

    [AGREGA] un componente para la vista de contacto prevista

--- Agenda Shufle/Agenda Shufle/PantallaAgenda.swift
+++ Agenda Shufle/Agenda Shufle/PantallaAgenda.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 struct PantallaAgenda: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        List {
+            VStack {
+                ForEach(0...45, id: \.self){ _ in
+                    ContactoPrevista()
+                }
+            }
+            .background(Color.cyan)
+        }
+
     }
 }
+
+
+
 
 #Preview {
     PantallaAgenda()
---
+++ Agenda Shufle/Agenda Shufle/componentes/contacto_prevista.swift
@@ -0,0 +1,22 @@
+//
+//  contacto_prevista.swift
+//  Agenda Shufle
+//
+//  Created by Jadzia Gallegos on 26/02/25.
+//
+
+import SwiftUI
+
+struct ContactoPrevista: View {
+    var body: some View {
+        HStack{
+            Image(systemName: "globe")
+            Text("Hello Darknes")
+            Text("My old friend")
+        }
+    }
+}
+
+#Preview {
+    ContactoPrevista()
+}
