0f891096fd8159eac8294d186d927c9a31488928
Jadzia Eleonore <bluakorvo@protonmail.com>
2025-02-26 09:46:09 -0700

    [INICIALIZA] el proyecto de xcode

---
+++ Agenda Shufle/Agenda Shufle/Agenda_ShufleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Agenda_ShufleApp.swift
+//  Agenda Shufle
+//
+//  Created by Jadzia Gallegos on 26/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Agenda_ShufleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PantallaAgenda()
+        }
+    }
+}
---
+++ Agenda Shufle/Agenda Shufle/PantallaAgenda.swift
@@ -0,0 +1,18 @@
+//
+//  PantallaAgenda.swift
+//  Agenda Shufle
+//
+//  Created by Jadzia Gallegos on 26/02/25.
+//
+
+import SwiftUI
+
+struct PantallaAgenda: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PantallaAgenda()
+}
